78875f3d7d3ca55f35d27a00ae305bf9b0bd8936
Aseel Al Dallal <asoola@Aseels-MacBook-Air.local>
2020-02-14 12:17:22 -0500

    connect redis

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,12 @@ import express from "express";
 import redis from "redis";
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+  // Usually we put in address here. We are using docker compose,
+  // so we can use service name
+  host: "redis-server",
+  port: 6379
+});
 client.set("visits", "0");
 
 app.get("/", (req, res) => {
