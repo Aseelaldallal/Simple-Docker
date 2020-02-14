939fdb92a4f60b5e85da4363698b0d06540f847d
Aseel Al Dallal <asoola@Aseels-MacBook-Air.local>
2020-02-13 23:09:10 -0500

    redis

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,15 @@ import redis from "redis";
 
 const app = express();
 const client = redis.createClient();
+client.set("visits", "0");
+
+app.get("/", (req, res) => {
+  client.get("visits", (err, visits) => {
+    res.send("Number of visits is " + visits);
+    client.set("visits", (+visits + 1).toString());
+  });
+});
+
+app.listen(8081, () => {
+  console.log("Listening on port 8081");
+});
