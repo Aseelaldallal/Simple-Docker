b80468626d0742d2c20312133c59b056f5ba438c
Aseel Al Dallal <asoola@Aseels-MacBook-Air.local>
2020-02-28 18:04:13 -0500

    docker compose

--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hi");
 var express_1 = __importDefault(require("express"));
 var redis_1 = __importDefault(require("redis"));
 var app = express_1.default();
-var client = redis_1.default.createClient();
+var client = redis_1.default.createClient({
+    // Usually we put in address here. We are using docker compose,
+    // so we can use service name
+    host: "redis-server",
+    port: 6379
+});
+client.set("visits", "0");
+app.get("/", function (req, res) {
+    process.exit(0);
+    client.get("visits", function (err, visits) {
+        res.send("Number of visits is " + visits);
+        client.set("visits", (+visits + 1).toString());
+    });
+});
+app.listen(8081, function () {
+    console.log("This is me");
+});
